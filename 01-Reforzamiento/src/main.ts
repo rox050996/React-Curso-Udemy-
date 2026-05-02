7718a8ce62d6368f769bcc5a75e84c2999bee8a3
Rox Mendoza <31557460+rox050996@users.noreply.github.com>
2026-05-02 15:15:09 -0600

    heroes data

--- 01-Reforzamiento/src/main.ts
+++ 01-Reforzamiento/src/main.ts
@@ -4,6 +4,7 @@ import './bases/01-const-let'
 import './bases/02-template-string'
 import './bases/03-object-literal'
 import './bases/04-arrays'
+import './bases/05-functions'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="hero">
